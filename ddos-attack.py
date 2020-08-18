a2ef01db7b46a2d7389dbfe8f77af70eae0d0591
cau-flare <54187816+cau-flare@users.noreply.github.com>
2020-08-18 20:04:11 -0300

    Update ddos-attack.py

--- ddos-attack.py
+++ ddos-attack.py
@@ -14,7 +14,7 @@ year = now.year
 
 ##############
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-bytes = random._urandom(100000)
+bytes = random._urandom(50000)
 #############
 
 os.system("clear")
